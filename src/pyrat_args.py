0cdf517b68d814dd9b0e295fb1d5d74256667a2b
Johansmm <jmejia_26@hotmail.com>
2024-03-11 23:21:33 +0100

    feat: handle pyrat args
    Script to load pyrat args without print help,
    in order to append new ones

---
+++ src/pyrat_args.py
@@ -0,0 +1,23 @@
+__all__ = ["pyrat", "parser"]
+
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+
+@contextmanager
+def suppress_output():
+    # Redirect stdout, stderr and exit to temporal objects
+    old_stdout, old_stderr, old_exit = sys.stdout, sys.stderr, sys.exit
+    sys.stdout, sys.stderr, sys.exit = StringIO(), StringIO(), lambda *args: None
+    try:
+        yield
+    finally:
+        # Restore original objects
+        sys.stdout, sys.stderr, sys.exit = old_stdout, old_stderr, old_exit
+
+
+# Import pyrat arguments inside of suppress_output context to support new arguments
+with suppress_output():
+    import pyrat
+    parser = pyrat.parser
