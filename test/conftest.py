27c100295fc566f3b38d12d5578fd8dc809e832a
Johansmm <jmejia_26@hotmail.com>
2024-03-19 22:31:11 +0100

    fix: remove root on linux path
    Test will run in Windows and Linux

--- test/conftest.py
+++ test/conftest.py
@@ -14,11 +14,9 @@ def turn(*args):
 
 def from_path_to_program(path):
     # A program must follow the format <package>.<subpackage>...<program>
-    program = os.path.splitext(str(path))[0].replace(os.sep, ".")
-    if program.startswith("C:."):
-        # Remove 'C:' in windows
-        program = program[3:]
-    return program
+    program = os.path.splitext(os.path.abspath(str(path)))[0].split(os.sep)
+    # Remove root directory (Windows 'C:' - Linux '/')
+    return ".".join(program[1:])
 
 
 @pytest.fixture(scope="session")
